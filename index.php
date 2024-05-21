d4a34d83bfdb9c555de20afb4a0f9f31aa774bea
Caioestevao100 <caioestevao_@hotmail.com>
2024-05-21 12:52:30 -0300

    adicionado os calculos e com resultados, porém falta adicionar a função

--- index.php
+++ index.php
@@ -5,61 +5,48 @@
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Atividade Avaliativa</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+
+    <style>
+
+        body {
+            text-align: center;
+            margin-top:  ;
+        }
+
+    </style>
+
 </head>
 <body>
 
     <?php
 
-        //Valores
-        $semanas = array(5, 2 -4, 0, 3);
-
-        function valores(){
-
-            //----------------------parOUimpar---------------------------------------------
+        $valores = array(5, 2, -4, 0, 3);
+        foreach($valores as $num){
+            //----------------------parOUimpar-------------------------------------------
             if($num % 2 == 0){
-                echo "O número informado é PAR";
-                echo "<br>DIGITE UM NOVO NÚMERO!";
-            }else{
-                echo "O número informado é IMPAR";
-                echo "<br>DIGITE UM NOVO NÚMERO!";
-            }
-            
-
-            //---------------------intOUfloat------------------------------------------------
-            if ($numero % 10 == 0){
-                echo "Número redondo";
-            }
-            else{
-                echo "Número não redondo";
+                    echo " $num é PAR,";
+                }else{
+                    echo " $num é IMPAR,";
             }
 
+            //---------------------intOUfloat---------------------------------------------
+            if ($num % 10 == 0){
+                    echo " redondo";
+                }
+                else{
+                    echo " não redondo";
+            }
 
-            //------------------------------+-0---------------------------------------------
-            $num = $_POST["num"];
-
+            //------------------------------+-0-------------------------------------------
             if($num > 0 ){
-                echo "O número informado é Positivo";
-            }if($num < 0){
-                echo "O número informado é Negativo";
-            }if($num == 0){
-                echo "O número é Neutro";
-            }
+                    echo " e Positivo.<br>";
+                }if($num < 0){
+                    echo " e Negativo.<br>";
+                }if($num == 0){
+                    echo "e Neutro.<br>";
+                }   
         }
 
-        
-        //Execução da Função
-        valores($n1, $n2, $n3, $n4, $n5);
-
-
-
-        /*numeros = [5, 2, -4, 0, 3]
-        O número 5 é impar positivo
-        O número 2 é par positivo.
-        O número -4 é par negativo.
-        O número zero é neutro.
-        O número 3 é impar positivo.*/
-        
     ?>
 
 </body>
